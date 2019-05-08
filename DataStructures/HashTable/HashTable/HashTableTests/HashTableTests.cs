fceaa067a1def4dc5edd3c03dba2e0c9892d1e05
kappz <addcolor117@gmail.com>
2019-05-08 13:29:35 -0400

    refactored tests

--- DataStructures/HashTable/HashTable/HashTableTests/HashTableTests.cs
+++ DataStructures/HashTable/HashTable/HashTableTests/HashTableTests.cs
@@ -5,31 +5,29 @@ namespace HashTableProjectTests
 {
     public class HashTableUnitTests
     {
+        HashTable hashTable;
+
         [SetUp]
         public void Setup()
         {
+            hashTable = new HashTable(1333333);
         }
 
         [Test]
         public void HashTableCreatedSuccessfully()
         {
-            HashTable hashTable = new HashTable(1333333);
             Assert.AreEqual(hashTable.GetSize(), 1333333);
         }
 
         [Test]
         public void NegativeOneReturnedIfNoMappingForKeyExists()
         {
-            HashTable hashTable = new HashTable(1333333);
-
             Assert.AreEqual(-1, hashTable.Get(1337));
         }
 
         [Test]
         public void HashTableCorrectlyHashesKeyIntoValue()
         {
-            HashTable hashTable = new HashTable(1333333);
-
             Assert.AreEqual(1234, hashTable.Hash(1234));
         }
     }
