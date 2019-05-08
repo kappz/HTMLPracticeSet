043f3d96703cdd991c0f2b029312bb1463cd2b94
kappz <addcolor117@gmail.com>
2019-05-08 13:15:55 -0400

    Hash table creation test passes

---
+++ DataStructures/HashTable/HashTable/HashTable/HashTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HashTableProject
+{
+    public class HashTable
+    {
+        private int size;
+        private int[] table;
+
+        public HashTable(int s = 0)
+        {
+            size = s;
+            table = new int[size];
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                table[i] = -1;
+            }
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
+    }
+}
---
+++ DataStructures/HashTable/HashTable/HashTableTests/HashTableTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using HashTableProject;
+
+namespace HashTableProjectTests
+{
+    public class HashTableUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void HashTableCreatedSuccessfully()
+        {
+            HashTable hashTable = new HashTable(1333333);
+            Assert.AreEqual(hashTable.GetSize(), 1333333);
+        }
+
+        [Test]
+        public void NegativeOneReturnedIfNoMappingForKeyExists()
+        {
+            HashTable hashTable = new HashTable(1333333);
+
+            Assert.AreEqual(-1, hashTable.get(1337));
+        }
+    }
+}
