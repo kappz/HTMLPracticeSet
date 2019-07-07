9f0760c079897e15eeafbd611a1c6b2dee121d68
kappz <addcolor117@gmail.com>
2019-07-07 19:37:12 -0400

    implemented minHeap data structure.

--- DataStructures/Heap/Heap/Heap.cs
+++ DataStructures/Heap/Heap/Heap.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Heap
 {
@@ -67,19 +68,132 @@ namespace Heap
             }
         }
 
-        public T GetLeftChild(int parent)
+        private T GetLeftChild(int parent)
         {
             return heap[GetLeftIndex(parent)];
         }
 
-        public T GetRightChild(int parent)
+        private T GetRightChild(int parent)
         {
             return heap[GetRightIndex(parent)];
         }
 
-        public T GetParent(int child)
+        private T GetParent(int child)
         {
             return heap[GetParentIndex(child)];
+        }
+
+        private void Swap(int indexOne, int indexTwo)
+        {
+            T temp = heap[indexOne];
+            heap[indexOne] = heap[indexTwo];
+            heap[indexTwo] = temp;
+        }
+
+        private void ExpandCapacity()
+        {
+            T[] arr;
+
+            if (size == capacity)
+            {
+                arr = new T[capacity * 2];
+                for (int i = 0; i < size; i++)
+                {
+                    arr[i] = heap[i];
+                }
+                heap = arr;
+                capacity *= 2;
+            }
+        }
+
+        public T Peek()
+        {
+            if (size == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                return heap[0];
+            }
+        }
+
+        public T Remove()
+        {
+            T element;
+
+            if (size == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                element = heap[0];
+                heap[0] = heap[size - 1];
+                --size;
+                HeapifyDown();
+                return element;
+            }
+        }
+
+        public void add(T element)
+        {
+            ExpandCapacity();
+            heap[size] = element;
+            ++size;
+            HeapifyUp();
+        }
+
+        public void HeapifyUp()
+        {
+            int index = size - 1;
+            while (HasParent(index) && isGreaterThan(GetParent(index), heap[index]))
+            {
+                Swap(GetParentIndex(index), index);
+                index = GetParentIndex(index);
+            }
+        }
+
+        public void HeapifyDown()
+        {
+            int index = 0;
+            int smallerIndex;
+            while(HasLeftChild(index))
+            {
+                smallerIndex = GetLeftIndex(index);
+                if (HasRightChild(index) && !(isGreaterThan(GetRightChild(index), GetLeftChild(index))))
+                {
+                    smallerIndex = GetRightIndex(index);
+                }
+
+                if (!isGreaterThan(heap[index], heap[smallerIndex]))
+                {
+                    break;
+                }
+                else
+                {
+                    Swap(index, smallerIndex);
+                }
+                index = smallerIndex;
+            }
+        }
+        private bool isGreaterThan(T value, T other)
+        {
+            var valueType = value.GetType();
+
+            if (valueType == typeof(int))
+            {
+                int valueChar = (int)(object)value;
+                int otherChar = (int)(object)value;
+                return valueChar > otherChar;
+            }
+            if (valueType == typeof(char))
+            {
+                int valueInt = (int)(object)value;
+                int otherInt = (int)(object)value;
+                return valueInt > otherInt;
+            }
+            return false;
         }
     }
 }
