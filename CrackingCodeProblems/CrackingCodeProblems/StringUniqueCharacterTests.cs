86ac791997a223aa3d9c7abcdc862a1e5d62cf36
kappz <addcolor117@gmail.com>
2019-07-11 14:37:05 -0400

    git ignore created

---
+++ CrackingCodeProblems/CrackingCodeProblems/StringUniqueCharacterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace CrackingCodeProblems
+{
+    class StringUniqueCharacterTests
+    {
+
+        [Test]
+        public void StringUniqueCharacters_ContainsOnlyUniqueCharacters_ReturnsFalseIfStringIsNull()
+        {
+            // Arrange
+            // Act
+            StringUniqueCharacters text = new StringUniqueCharacters("");
+
+
+            // Assert
+            text.ContainsOnlyUniqueCharacters().Should().BeFalse();
+        }
+    }
+}
