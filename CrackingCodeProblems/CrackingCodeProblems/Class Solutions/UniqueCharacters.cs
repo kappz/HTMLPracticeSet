250c5b488e5fde689f1993c3c39c8558a627f931
kappz <addcolor117@gmail.com>
2019-07-11 14:38:36 -0400

    Created project for Cracking the Code Interview Problems. Derived solution for unique characters in a string. Solution Passing tests.

---
+++ CrackingCodeProblems/CrackingCodeProblems/Class Solutions/UniqueCharacters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingCodeProblems.Class_Solutions
+{
+    class UniqueCharacters
+    {
+        string text;
+
+        public UniqueCharacters(string t)
+        {
+            text = t;
+        }
+
+        public bool ContainsOnlyUniqueCharacters()
+        {
+            bool result = true;
+            IDictionary<char, int> characterFrequencies = new Dictionary<char, int>();
+
+            if (text == null)
+            {
+                result = false;
+            }
+            else
+            {
+                foreach(char character in text)
+                {
+                    if (!characterFrequencies.ContainsKey(character))
+                    {
+                        characterFrequencies.Add(character, 1);
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CrackingCodeProblems/CrackingCodeProblems/Solution Tests/UniqueCharactersTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+using CrackingCodeProblems.Class_Solutions;
+
+namespace CrackingCodeProblems.Solution_Tests
+{
+    class UniqueCharactersTests
+    {
+
+        [Test]
+        public void UniqueCharacters_ContainsOnlyUniqueCharactes_ReturnsFalseIfTextIsNull()
+        {
+            // Arrange
+            bool actual;
+            bool expected = false;
+            UniqueCharacters text = new UniqueCharacters("");
+
+            // Act
+            actual = text.ContainsOnlyUniqueCharacters();
+
+            // Assert
+            actual.Should().Equals(expected);
+        }
+
+        [Test]
+        public void UniqueCharacters_ContainsOnlyUniqueCharactes_ReturnsTrueIfStringContainsOnlyUniqueCharacters()
+        {
+            // Arrange
+            bool actual;
+            bool expected = true;
+            UniqueCharacters text = new UniqueCharacters("test");
+
+            // Act
+            actual = text.ContainsOnlyUniqueCharacters();
+
+            // Assert
+            actual.Should().Equals(expected);
+        }
+
+        [Test]
+        public void UniqueCharacters_ContainsOnlyUniqueCharactes_ReturnsFalseIfStringContainsDuplicateCharacters()
+        {
+            // Arrange
+            bool actual;
+            bool expected = false;
+            UniqueCharacters text = new UniqueCharacters("test");
+
+            // Act
+            actual = text.ContainsOnlyUniqueCharacters();
+
+            // Assert
+            actual.Should().Equals(expected);
+        }
+    }
+}
